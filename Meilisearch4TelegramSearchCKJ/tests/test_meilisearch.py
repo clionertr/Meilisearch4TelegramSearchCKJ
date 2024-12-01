d78587e8e6da958658c4a5c64b16adb4bd0613e4
clionertr <clionertr@qq.com>
2024-12-01 10:42:45 +0800

    优化搜索缓存

--- Meilisearch4TelegramSearchCKJ/tests/test_meilisearch.py
+++ Meilisearch4TelegramSearchCKJ/tests/test_meilisearch.py
@@ -1,9 +1,20 @@
-import meilisearch
+# import meilisearch
+# search_results_cache = {}
+# from Meilisearch4TelegramSearchCKJ.src.config.env import MEILI_HOST, MEILI_PASS
+# from Meilisearch4TelegramSearchCKJ.src.models.meilisearch_handler import MeiliSearchClient
+#
+# meili = MeiliSearchClient(MEILI_HOST, MEILI_PASS)
+# meili.create_index()
+# result = meili.search( "hello","telegram",limit=1)
+# search_results_cache["helllo"] = result["hits"][0]
+# results = search_results_cache["helllo"]
 
-from Meilisearch4TelegramSearchCKJ.src.config.env import MEILI_HOST, MEILI_PASS
-from Meilisearch4TelegramSearchCKJ.src.models.meilisearch_handler import MeiliSearchClient
+# print(dict1)
+# print(search_results_cache)
+# print(result)
+# print(results)
 
-meili = MeiliSearchClient(MEILI_HOST, MEILI_PASS)
-meili.create_index()
-result = meili.search("telegram", "hello",limit=1)
-print(result)
+dict1 = {"hello": [{"id": 1, "text": "hello world"}]}
+print(dict1.get("hello"))
+dict1["hello"].extend([{"id": 2, "text": "hello world2"}])
+print(dict1.get("hello"))
