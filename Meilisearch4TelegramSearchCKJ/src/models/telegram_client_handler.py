79764e67fac792cdaf5295a5a129cd9e205bbab1
clionertr <clionertr@qq.com>
2024-12-03 20:44:45 +0800

    防止日志过多

--- Meilisearch4TelegramSearchCKJ/src/models/telegram_client_handler.py
+++ Meilisearch4TelegramSearchCKJ/src/models/telegram_client_handler.py
@@ -283,6 +283,6 @@ class TelegramUserBot:
     def get_memory_usage():
         """获取内存使用情况"""
         current, peak = tracemalloc.get_traced_memory()
-        logger.info(f"Current memory usage: {current / 10 ** 6}MB")
-        logger.info(f"Peak memory usage: {peak / 10 ** 6}MB")
+        logger.debug(f"Current memory usage: {current / 10 ** 6}MB")
+        logger.debug(f"Peak memory usage: {peak / 10 ** 6}MB")
         return current, peak
