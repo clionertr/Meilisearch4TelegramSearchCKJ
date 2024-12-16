bd09062c6dad5b0d98561333dce8c3468e847fbd
clionertr <clionertr@qq.com>
2024-12-16 22:12:53 +0800

    更新env.py

--- Meilisearch4TelegramSearchCKJ/src/config/env.py
+++ Meilisearch4TelegramSearchCKJ/src/config/env.py
@@ -10,7 +10,7 @@ SESSION_STRING = os.getenv("SESSION_STRING", None)
 # 同步ID设置
 # 同步历史消息的群组/对话/用户 ID，如果为只有一个ID，请在末尾加上逗号。如果为空，设置为()
 # 开启白名单后，黑名单失效
-WHITE_LIST = ast.literal_eval(os.getenv("WHITE_LIST","()"))
+WHITE_LIST = ast.literal_eval(os.getenv("WHITE_LIST","(1,)"))
 BLACK_LIST = ast.literal_eval(os.getenv("BLACK_LIST", "()"))
 
 # MeiliSearch 设置
