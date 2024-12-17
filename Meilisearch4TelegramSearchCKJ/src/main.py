1a423d3d5baca093255182b7617be37277c16f60
clionertr <clionertr@qq.com>
2024-12-17 11:06:29 +0800

    取消开启用户机器人发送的群组信息

--- Meilisearch4TelegramSearchCKJ/src/main.py
+++ Meilisearch4TelegramSearchCKJ/src/main.py
@@ -20,12 +20,12 @@ async def main(neo_msg):
         async for d in user_bot_client.client.iter_dialogs():
             # 便于获取对话的 id 和标题
             logger.log(25, f"Dialogs: {d.id}, {d.title if d.title else d }")
-            addtion_text = f"\n对话ID: {d.id}, 对话标题：{d.title if d.title else d }"
-            new_text = neo_msg.text + addtion_text
-            if len(new_text) > 3000:
-                neo_msg = await neo_msg.reply("消息过长，已截断")
-                new_text = addtion_text
-            neo_msg = await neo_msg.edit(new_text)
+            # addtion_text = f"\n对话ID: {d.id}, 对话标题：{d.title if d.title else d }"
+            # new_text = neo_msg.text + addtion_text
+            # if len(new_text) > 3000:
+            #     neo_msg = await neo_msg.reply("消息过长，已截断")
+            #     new_text = addtion_text
+            # neo_msg = await neo_msg.edit(new_text)
             if WHITE_LIST:
                 if d.id in WHITE_LIST:
                     logger.info(f"Downloading history for {d.title or d.id}")
