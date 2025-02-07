b37e4f19be11c2c264bcf1a23ca82d0eb3bc3079
clionertr <clionertr@qq.com>
2025-02-07 14:31:43 +0800

    更新了获取string session

--- Meilisearch4TelegramSearchCKJ/tests/get_str_session.py
+++ Meilisearch4TelegramSearchCKJ/tests/get_str_session.py
@@ -1,10 +1,9 @@
+import os
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-from Meilisearch4TelegramSearchCKJ.src.config.env import APP_ID, APP_HASH
-
-api_id = APP_ID
-api_hash = APP_HASH
+api_id = os.getenv('APP_ID', '12345')
+api_hash = os.getenv('APP_HASH', '0123456789abcdef0123456789abcdef')
 with TelegramClient(StringSession(), api_id, api_hash) as client:
     print('以下是你的string session（类似登陆密钥）\n')
     print(client.session.save())
