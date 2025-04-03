ec05397918028cbde006cdc6ecf9e6cd7ef674e2
clionertr <clionertr@qq.com>
2025-04-03 22:48:27 +0800

    更新utlils

--- Meilisearch4TelegramSearchCKJ/src/utils/get_memory_usage.py
+++
@@ -1,8 +0,0 @@
-import tracemalloc
-
-def get_memory_usage(logger):
-        """获取内存使用情况"""
-        current, peak = tracemalloc.get_traced_memory()
-        logger.info(f"Current memory usage: {current / 10 ** 6}MB")
-        logger.info(f"Peak memory usage: {peak / 10 ** 6}MB")
-        return current, peak
